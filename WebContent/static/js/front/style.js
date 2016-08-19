609a50279397d8b43a8631a91a22c0e530f0bb58
xueyunteng <464380730@qq.com>
2016-08-19 15:04:05 +0800

    1 iframe自适应 2 去除时间初始化 3 初始化方法整合

--- WebContent/static/js/front/style.js
+++ WebContent/static/js/front/style.js
@@ -1,39 +1,16 @@
-$(function(){
-	var timeBet=new Array();
-	var innerHtml='';
-	timeBet.push('<input class="Wdate write-time input-text radius size-M"  type="text"  onFocus="WdatePicker({dateFmt:&quot;yyyy-MM-dd HH:mm:ss&quot;,isShowClear:false,readOnly:true})"/>');
-	innerHtml=timeBet.join('');
-	$('.timeAlone').append(innerHtml);
-});
 
-$(function(){
-	var timeBet=new Array();
-	var innerHtml='';
-	timeBet.push('<input id="d4311" placeholder="请输入开始时间" class="Wdate write-time input-text radius size-M" type="text" onFocus="WdatePicker({dateFmt:&quot;yyyy-MM-dd HH:mm&quot;,maxDate:&quot;#F{$dp.$D(\'d4312\')||\'2020-10-01\'}&quot;})"/> '+
-         '<span class="sprite">-</span>'+
-         '<input id="d4312" placeholder="请输入结束时间" class="Wdate write-time input-text radius size-M" type="text" onFocus="WdatePicker({dateFmt:&quot;yyyy-MM-dd HH:mm&quot;,minDate:&quot;#F{$dp.$D(\'d4311\')}&quot;,maxDate:&quot;2020-10-01&quot;})"/>' 
-         );
-	innerHtml=timeBet.join('');
-	$('.timeBet').append(innerHtml);
-});
-
-$(function(){
-	setIframeHeight(document.getElementById('external-frame'));
-});
-
-// iframe高度自适应
-function setIframeHeight(iframe) {
-	if (iframe) {
-		var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
-		if (iframeWin.document.body) {
-			iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
-		}
-	}
+function setIframeHeightChild(IframeId){
+	$(window.parent.document).find('#'+IframeId).map(function(){
+    var main = $(window.parent.document).find('#'+IframeId);
+    var thisheight = $('body').height()+100;
+    main.height(thisheight);
+    });
 }
 
-// JavaScript Document
-$(document).ready(function(e) {
-    $('.stage-nav li').click(function(){
+
+$(function(){
+	
+	$('.stage-nav li').click(function(){
 		$(this).addClass('li-h2').siblings().removeClass('li-h2');
 	});
 	$('.search-nav-l li').click(function(){
@@ -48,17 +25,20 @@ $(document).ready(function(e) {
 	$('.read-nav li').click(function(){
 		$(this).addClass('li-h6').siblings().removeClass('li-h6');
 	});
-});
-
-// 导航选中效果
-$(function(){
+	
+	//子页面调用iframe自适应方法
+	var iframeId = $(window.parent.document).find("#external-frame").attr('id');
+	$('body').addClass('clearfix');
+	if($('body div:first').hasClass("PersonalCenter-main-r")){
+		setIframeHeightChild(iframeId);
+	}
+	
+	// 导航选中效果
 	$('.menuSidebar dl dd').on('click',function(){
 		$(this).addClass('active').siblings().removeClass('active');
 		return false;
 	})
-})
-
-$(function(){
+	
 	// 添加选中项
 	$('.tab-tip').click(function(){
 		$(this).addClass('active').siblings().removeClass('active');
@@ -71,5 +51,4 @@ $(function(){
 	$(function(){$(window).on("scroll",$backToTopFun);$backToTopFun();});
 	// tab切换
 	$.Huitab("#tab_detail .tabBar span","#tab_detail .tabCon","current","click","0");
-	 
-});
+})
